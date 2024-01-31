65f8e079195a00ab87a2f90b718c6a934e5d9f0c
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-01-31 23:21:32 +0100

    change item to n in the structer

--- binary_trees.h
+++ binary_trees.h
@@ -7,14 +7,14 @@
 /**
  * struct binary_tree_s - Binary tree node
  *
- * @item: Integer stored in the node
+ * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
 struct binary_tree_s
 {
-	int item;
+	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
