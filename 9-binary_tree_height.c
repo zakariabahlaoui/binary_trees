2f9f6452542344ab3e65254211120e4452f2185d
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-02-01 04:07:19 +0100

    Refactor binary_tree_height function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,11 +9,19 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (0);
+		return 0;
 
-	size_t L = 0, R = 0;
+	size_t l = 0;
+	size_t r = 0;
 
-	L = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	R = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return ((L > R) ? L : R);
+	if (tree->left != NULL)
+		l = 1 + binary_tree_height(tree->left);
+
+	if (tree->right != NULL)
+		r = 1 + binary_tree_height(tree->right);
+
+	if (l > r)
+		return l;
+	else
+		return r;
 }
