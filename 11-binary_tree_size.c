c7f4a9a3f64b900baa34bc0b410f9ff41c2f07d9
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-02-01 22:43:20 +0100

    fix: betty

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -5,19 +5,20 @@
  *
  * Return: size of tree
  */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    size_t size = 0, r = 0, l = 0;
+	size_t size = 0, r = 0, l = 0;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    else
-    {
-        l = binary_tree_size(tree->left);
-        r = binary_tree_size(tree->right);
-        size = r + l + 1;
-    }
-    return (size);
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		l = binary_tree_size(tree->left);
+		r = binary_tree_size(tree->right);
+		size = r + l + 1;
+	}
+	return (size);
 }
