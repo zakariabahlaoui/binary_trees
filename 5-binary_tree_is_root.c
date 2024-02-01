afcd11b94631f502ceff48a28584e4451ac51432
Yassmineattar <yassmineattar13@gmail.com>
2024-02-01 02:26:00 +0100

    a

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,7 +8,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	if (node->left != NULL || node->right != NULL)
+	if (node->parent != NULL)
 		return (0);
 	else
 		return (1);
