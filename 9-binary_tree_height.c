111569449f4c6b6178a7511db6b8cb29e69b9f60
Yassmineattar <yassmineattar13@gmail.com>
2024-02-01 03:31:21 +0100

    fixing indentation error & testing

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,9 +11,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-		size_t L = 0, R = 0;
+	size_t L = 0, R = 0;
 
-		L = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		R = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		return ((L > R) ? L : R);
+	L = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	R = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return ((L > R) ? L : R);
 }
