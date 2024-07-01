8109d1d0d5586ba14d0b4e57d38e358c5e4bd237
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-07-01 02:29:21 +0100

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a binary tree
+ * binary_tree_height - measures  the height of a binary tree
  * @tree: pointer to the root node of the tree
  * Return: height of the tree
  */
