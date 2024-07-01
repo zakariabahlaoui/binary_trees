6763546bd1b6e1024bcf78cc45bc291bda8b1214
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-07-01 02:30:31 +0100

    14-binary_tree_balance.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 /**
-* binary_tree_is_leaf - checks if a node is a leaf
-* @node: pointer to the node to check
-* Return: return 1 if node is a leaf, otherwise 0
-*/
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to the node to check
+ * Return: return 1 if node  is a leaf, otherwise 0
+ */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
